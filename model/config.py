eb9f62d0e58161dc1fe2c04f2ca7a61aca6c5e3e
guixiangyu <guixiangyu1@gmail.com>
2019-04-21 13:58:21 +0800

    change data pipeline

--- model/config.py
+++ model/config.py
@@ -95,7 +95,7 @@ class Config():
     train_embeddings = False
     nepochs          = 30
     dropout          = 0.5
-    batch_size       = 200
+    batch_size       = 500
     lr_method        = "adam"
     lr               = 0.0001
     lr_decay         = 1.0
